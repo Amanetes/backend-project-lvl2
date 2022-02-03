ebe77f6f4e7d76ac47ebf5ce2574f94b26fe55d1
Sergey <ser_8904@mail.ru>
2022-02-03 11:09:28 +0300

    program initialization

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
+program.parse();

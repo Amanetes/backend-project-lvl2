ccb283ef633297f9bd45eaab72340ce754a9b531
Sergey <ser_8904@mail.ru>
2022-02-04 10:08:02 +0300

    update gendiff with reduce

--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 // Функция читает путь файлов и возвращает отличия
+// readFileSync читает относительный путь до файла
+// path.resolve - строит абсолютный путь
 
-const genDiff = () => {
-  const filepath1 = JSON.parse(readFileSync(path.resolve(process.cwd(), '__fixtures__/file1.json'), 'utf8'));
-  const filepath2 = JSON.parse(readFileSync(path.resolve(process.cwd(), '__fixtures__/file2.json'), 'utf8'));
+const genDiff = (file1, file2) => {
+  const filepath1 = JSON.parse(readFileSync(path.resolve(process.cwd(), file1), 'utf8'));
+  const filepath2 = JSON.parse(readFileSync(path.resolve(process.cwd(), file2), 'utf8'));
   const merged = _.sortBy(Object.keys({ ...filepath1, ...filepath2 }));
-  const res = {};
-  for (const key of merged) {
+  // В пустой объект с помощью reduce добавляем ключи
+  return merged.reduce((acc, key) => {
     if (!Object.hasOwn(filepath1, key)) {
-      res[`+ ${key}`] = filepath2[key];
+      acc[`+ ${key}`] = filepath2[key];
     } else if (filepath1[key] === filepath2[key]) {
-      res[`  ${key}`] = filepath1[key];
+      acc[`  ${key}`] = filepath1[key];
     } else if (Object.hasOwn(filepath2, key)) {
-      res[`- ${key}`] = filepath1[key];
-      res[`+ ${key}`] = filepath2[key];
-    } else if (Object.hasOwn(filepath1, key)) {
-      res[`- ${key}`] = filepath1[key];
+      acc[`- ${key}`] = filepath1[key];
+      acc[`+ ${key}`] = filepath2[key];
+    } else if (Object.hasOwn(file1, key)) {
+      acc[`- ${key}`] = filepath1[key];
     }
-  }
-  return res;
+    return { ...acc };
+  }, {});
 };
 export default genDiff;

eee0dccca81b2306312882cbeb7a800383799823
Sergey <ser_8904@mail.ru>
2022-02-06 19:54:03 +0300

    step 6 preparation

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,14 +10,21 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-let testData1;
-let testData2;
+let testJson1;
+let testJson2;
 let expected;
+let testYml1;
+let testYml2;
 beforeEach(() => {
-  testData1 = getFixturePath('file1.json');
-  testData2 = getFixturePath('file2.json');
-  expected = readFile('expected_file.json');
+  testJson1 = getFixturePath('file1.json');
+  testJson2 = getFixturePath('file2.json');
+  expected = readFile('expected_file.txt');
+  testYml1 = getFixturePath('file1.yml');
+  testYml2 = getFixturePath('file2.yml');
 });
-test('file difference', () => {
-  expect(genDiff(testData1, testData2)).toEqual(expected);
+test('json difference check', () => {
+  expect(genDiff(testJson1, testJson2)).toEqual(expected);
+});
+test('yml difference check', () => {
+  expect(genDiff(testYml1, testYml2)).toEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
+import parseFile from './parsers.js';
 
-// Функция читает путь файлов и возвращает отличия
-// readFileSync читает относительный путь до файла
-// path.resolve - строит абсолютный путь
+const convertToString = (obj) => {
+  const stringifiedObj = Object.entries(obj).map((items) => items
+    .join(': ')).join('\n');
+  return `{\n${stringifiedObj} \n}`;
+};
 
 const genDiff = (filepath1, filepath2) => {
-  const data1 = JSON.parse(readFileSync(path.resolve(process.cwd(), filepath1), 'utf8'));
-  const data2 = JSON.parse(readFileSync(path.resolve(process.cwd(), filepath2), 'utf8'));
+  const data1 = parseFile(readFileSync(path.resolve(process.cwd(), filepath1), 'utf8'));
+  const data2 = parseFile(readFileSync(path.resolve(process.cwd(), filepath2), 'utf8'));
   const merged = _.sortBy(Object.keys({ ...data1, ...data2 }));
   // В пустой объект с помощью reduce добавляем ключи
   const result = merged.reduce((acc, key) => {
@@ -24,6 +27,6 @@ const genDiff = (filepath1, filepath2) => {
     }
     return acc;
   }, {});
-  return JSON.stringify(result, null, 2);
+  return convertToString(result);
 };
 export default genDiff;

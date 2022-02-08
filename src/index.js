a2ceeab2a4b95ff513c48f174e109f475ff53887
Sergey <ser_8904@mail.ru>
2022-02-08 09:39:55 +0300

    distinct diff func

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import parseFile from './parsers.js';
 import stylish from './formatters/stylish.js';
 import matchFiles from './matcher.js';
 
-export default ((filepath1, filepath2, format = stylish) => {
+export default ((filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(readFileSync(path.resolve(process.cwd(), filepath1), 'utf8'));
   const data2 = parseFile(readFileSync(path.resolve(process.cwd(), filepath2), 'utf8'));
 
-  const diff = matchFiles(data1, data2);
-  return format(diff);
+  const diff = matchFiles(data1, data2, format);
+  return stylish(diff);
 });

e48149b8213dba76d69a7944647d58bee1990b0a
Sergey <ser_8904@mail.ru>
2022-02-10 09:43:18 +0300

    update diff creation logic

--- src/matcher.js
+++ src/matcher.js
@@ -1,24 +1,29 @@
 import _ from 'lodash';
 
-const matchFiles = ((data1, data2) => {
+const matchFiles = (data1, data2) => {
   // Формирование массива уникальных ключей из 2х объектов
   const merged = _.sortBy(Object.keys({ ...data1, ...data2 }));
-  // Сборка объекта на основе сравнения ключей объектов и массива
-  const result = merged.reduce((acc, key) => {
+  // Выводим в результат новый массив ключей/значений
+  return merged.map((key) => {
+    // Записываем в результирующий массив - объекты
+    // с новым внутренним представлением(аналог - Esprima)
     if (!Object.hasOwn(data1, key)) {
-      acc[`+ ${key}`] = data2[key];
-    } else if (!Object.hasOwn(data2, key)) {
-      acc[`- ${key}`] = data1[key];
-    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      acc[`  ${key}`] = matchFiles(data1[key], data2[key]);
-    } else if (data1[key] !== data2[key]) {
-      acc[`- ${key}`] = data1[key];
-      acc[`+ ${key}`] = data2[key];
-    } else {
-      acc[`  ${key}`] = data1[key];
+      return { name: key, type: 'added', value: data2[key] };
     }
-    return acc;
-  }, {});
-  return result;
-});
+    if (!Object.hasOwn(data2, key)) {
+      return { name: key, type: 'deleted', value: data1[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { name: key, type: 'nested', children: matchFiles(data1[key], data2[key]) };
+    }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
+        name: key, type: 'changed', previousValue: data1[key], newValue: data2[key],
+      };
+    }
+    return {
+      name: key, type: 'similar', key, value: data1[key],
+    };
+  });
+};
 export default matchFiles;
